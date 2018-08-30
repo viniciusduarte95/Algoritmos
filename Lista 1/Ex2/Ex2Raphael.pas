7c487ce9764c8f9182bc9115e11bc605c46486ec
noh4nsen <42837453+noh4nsen@users.noreply.github.com>
2018-08-30 10:59:28 -0300

    Create Ex2Raphael.pas

---
+++ Lista 1/Ex2/Ex2Raphael.pas
@@ -0,0 +1,47 @@
+Program ex2;
+var
+    cod: integer;
+    preco: real;
+
+Begin
+  writeln('Digite o código de origem:' );
+  readln (cod);
+  writeln ('Digite o preço: ');
+  readln (preco);
+  clrscr;
+  
+  //write('preco: ');
+  //readln(preco);
+  if (cod=1) then
+  begin
+    writeln ('Sul', preco:10:2);
+  end
+  else
+   if (cod>1) and (cod<5) then
+ 	 begin
+   	 writeln ('Norte', preco:10:2);
+ 	 end
+ 	 else
+ 		 if (cod>4) and (cod<10) then
+ 		 begin
+   		 writeln ('Sudeste', preco:10:2);
+ 		 end
+ 		 else
+			if (cod>9) and (cod<14) then
+			begin
+   			 writeln ('Norte', preco:10:2);
+			end
+			else
+				if (cod>14) then
+				begin
+					writeln ('Outra região', preco:10:2);
+				end
+  
+  else
+  begin
+    writeln ('O produto é importado', preco:10:2);
+  end;
+  
+  write ('Aperte enter para encerrar');
+  readln;
+End.
